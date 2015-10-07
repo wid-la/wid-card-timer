e5f78741fb134a45354564182396404b9b2db76c
Aurelien Chaudagne <ouralien@gmail.com>
2015-10-06 17:52:18 -0700

    Styling of element.

--- wid-card-timer.js
+++ wid-card-timer.js
@@ -6,7 +6,40 @@
 
     is: 'wid-card-timer',
 
-    properties: {}
+    behaviors: [Polymer.WidCardStandardBehavior, Polymer.WidCardWeightBehavior, Polymer.WidCardSubscriptionsBehavior],
+
+    properties: {
+      /**
+       * title
+       * @type {String}
+       */
+      title: {
+        type: String,
+        value: 'Title'
+      },
+
+      /**
+       * value
+       * @type {String}
+       */
+      value: {
+        type: String,
+        value: 30
+      },
+
+      /**
+       * description
+       * @type {String}
+       */
+      description: {
+        type: String,
+        value: 'description'
+      }
+    },
+
+    _timeValue: function _timeValue(value) {
+      return value + ' h';
+    }
 
   });
 })();
